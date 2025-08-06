b27212507c0cecf57bca57c44f6230efe1557107
angelo madara <angelo.madara@aiqueinnovation.com>
2025-08-07 00:18:46 +0800

    fix worker

--- worker.js
+++ worker.js
@@ -12,12 +12,3 @@ async function handleEvent(event) {
     return new Response('Not Found', { status: 404 });
   }
 }
-
-// import { createApp } from './.output/server/index.mjs';
-
-// export default {
-//   async fetch(request, env, ctx) {
-//     const app = await createApp();
-//     return app.fetch(request, env, ctx);
-//   }
-// }
