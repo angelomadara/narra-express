5c3f2415d0a71ab90d46340487bf21fe5783ea46
angelo madara <angelo.madara@aiqueinnovation.com>
2025-09-24 00:07:48 +0800

    feat: remove xss-clean, use express-validator for XSS protection, keep helmet

--- src/utils/rate-limiter.ts
+++ src/utils/rate-limiter.ts
@@ -21,7 +21,7 @@ export const userBasedLimiter = rateLimit({
   },
   keyGenerator: (req) => {
     let token = req.headers['authorization'] as string;
-    token = token.replace("Bearer ","")
+    token = token.replace("Bearer ", "")
     // Ensure we always return a string
     if (token) {
       const key = `${req.ip}.${token}`;
@@ -32,4 +32,23 @@ export const userBasedLimiter = rateLimit({
     console.log(fallbackKey)
     return fallbackKey;
   }
+});
+
+
+// strict rate limiter for login attempts
+export const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // limit each IP to 5 login attempts per windowMs
+  message: 'Too many login attempts, please try again later.',
+  handler: (req, res, next, options) => {
+    res.status(options.statusCode).json({ message: options.message });
+  },
+  skipSuccessfulRequests: true, // don't count successful requests towards the limit
 })
+
+// Rate limiting for password reset
+export const passwordResetLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour
+  max: 3, // Limit each IP to 3 password reset requests per hour
+  message: 'Too many password reset attempts, please try again later.',
+});
